2009c0d9f25f7c39a1e61ecea72892187e34c004
kPanihin <k.panihin@akolchin.com>
2018-11-26 15:43:05 +0300

    Заменил DispatchServletModule на MyServletModule. Request static injection OfyService. Установил MyHandlerModule.

--- src/main/java/com/project/car/server/guice/ServerModule.java
+++ src/main/java/com/project/car/server/guice/ServerModule.java
@@ -1,9 +1,14 @@
 package com.project.car.server.guice;
 
 import com.google.inject.AbstractModule;
+import com.project.car.server.dao.objectify.OfyService;
+import com.project.car.server.dispatch.MyHandlerModule;
 
 public class ServerModule extends AbstractModule {
     @Override
     protected void configure() {
+        requestStaticInjection(OfyService.class);
+
+        install(new MyHandlerModule());
     }
 }

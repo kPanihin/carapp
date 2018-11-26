4b8be847cfbbfc6bbed3cff871e0d35e952bc643
kPanihin <k.panihin@akolchin.com>
2018-11-26 15:31:44 +0300

    Подключил и инициализировал DispatchAsync

--- src/main/java/com/project/car/client/application/ApplicationPresenter.java
+++ src/main/java/com/project/car/client/application/ApplicationPresenter.java
@@ -1,18 +1,15 @@
 package com.project.car.client.application;
 
-import com.google.gwt.event.shared.GwtEvent;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.dispatch.rpc.shared.DispatchAsync;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
-import com.gwtplatform.mvp.client.annotations.ContentSlot;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.Proxy;
-import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
 import com.gwtplatform.mvp.client.presenter.slots.NestedSlot;
 
-public class ApplicationPresenter
-        extends Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy> {
+public class ApplicationPresenter extends Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy>{
     interface MyView extends View {
     }
 
@@ -22,11 +19,16 @@ public class ApplicationPresenter
 
     public static final NestedSlot SLOT_MAIN = new NestedSlot();
 
+    private final DispatchAsync dispatcher;
+
     @Inject
     ApplicationPresenter(
             EventBus eventBus,
             MyView view,
-            MyProxy proxy) {
+            MyProxy proxy,
+            DispatchAsync dispatcher) {
         super(eventBus, view, proxy, RevealType.Root);
+
+        this.dispatcher = dispatcher;
     }
 }

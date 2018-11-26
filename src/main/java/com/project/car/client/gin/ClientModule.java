2a30debf0e30058f3790fde7581e6004b87ee7ad
kPanihin <k.panihin@akolchin.com>
2018-11-26 15:38:35 +0300

    Установил RpcDispatchAsyncModule

--- src/main/java/com/project/car/client/gin/ClientModule.java
+++ src/main/java/com/project/car/client/gin/ClientModule.java
@@ -1,15 +1,14 @@
 package com.project.car.client.gin;
 
+import com.google.gwt.inject.client.AbstractGinModule;
+import com.gwtplatform.dispatch.rpc.client.gin.RpcDispatchAsyncModule;
 import com.project.car.client.application.ApplicationModule;
 import com.project.car.client.place.NameTokens;
 import com.project.car.client.resources.ResourceLoader;
-import com.gwtplatform.mvp.client.annotations.DefaultPlace;
-import com.gwtplatform.mvp.client.annotations.ErrorPlace;
-import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.gin.DefaultModule;
 
-public class ClientModule extends AbstractPresenterModule {
+public class ClientModule extends AbstractGinModule {
     @Override
     protected void configure() {
         install(new DefaultModule
@@ -18,6 +17,7 @@ public class ClientModule extends AbstractPresenterModule {
                 .errorPlace(NameTokens.HOME)
                 .unauthorizedPlace(NameTokens.HOME)
                 .build());
+        install(new RpcDispatchAsyncModule());
         install(new ApplicationModule());
 
         bind(ResourceLoader.class).asEagerSingleton();
